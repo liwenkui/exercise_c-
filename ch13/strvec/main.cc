f9a7b3e90a9721e374cd8651556f8b5e2f4de36e
liwenkui <liwenkui0229@icloud.com>
2016-11-04 20:38:28 +0800

    update

--- ch13/strvec/main.cc
+++ ch13/strvec/main.cc
@@ -1,7 +1,18 @@
 #include "StrVec.h"
+#include <memory>
+#include <string>
+#include <iostream>
 using namespace std;
+
+allocator<string> StrVec::alloc;
 int main()
 {
-    StrVec s;
-    return 0;
+	StrVec s;
+	s.push_back("a");
+	s.push_back("b");
+	s.push_back("c");
+	s.push_back("d");
+	s.push_back("f");
+	cout << s.size() << " " << s.capacity() << endl;
+	return 0;
 }
